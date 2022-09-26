27d8bedb0c0f95d8ef052bdc2a4029f535126036
Mukhtar <mukhtar@radix-it.ru>
2022-09-26 17:44:12 +0300

    .

--- django_register/django_register/settings.py
+++ django_register/django_register/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'register_with_from',
 ]
 
 MIDDLEWARE = [
@@ -69,17 +69,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_register.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testbd',
+        'USER': 'radix-dev',
+        'PASSWORD' :'saider569',
+        'HOST':'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -99,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -111,7 +118,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
@@ -121,3 +127,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'register_with_from.User'
+AUTHENTICATION_BACKENDS = ('register_with_from.backends.AuthBackend',)
+
+
+REST_FRAMEWORK = {
+    # роегистрация идет только по токенам для проверки просто по логину паролю нужно закомитеть нижнее
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
--- django_register/register_with_from/models.py
+++ django_register/register_with_from/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
 
-# Create your models here.
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField('username', max_length=255, unique=True)
+    email = models.EmailField('email address', null=True, blank=True)
+    phone = models.CharField('phone number', max_length=30, null=True, blank=True)
+    date_joined = models.DateTimeField('date joined', auto_now_add=True)
+    is_active = models.BooleanField('active', default=False)
+    is_staff = models.BooleanField('staff', default=False)
+
+    is_verified = models.BooleanField('verified', default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+        unique_together = ('username', 'email', 'phone')
